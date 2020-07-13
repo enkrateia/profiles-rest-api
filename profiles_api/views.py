c65bd984cdd15a6b97329a9e4dec6157da2c25d1
Carlos Govea <cgovea0310@gmail.com>
2020-07-13 18:22:08 -0500

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,91 +4,100 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
 
+
 class HelloApiView(APIView):
-  """Test API View"""
-  serializer_class = serializers.HelloSerializer
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
-  def get(self, request, format=None):
-    """Return a list of APIView features"""
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
 
-    an_apiview = [
-      'Uses HTTP methods as function (get, post, patch, put, delete)',
-      'Is similiar to a traditional Django view',
-      'Gives you the most control over you application logic',
-      'Gives you the most control over you application logic',
-    ]
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similiar to a traditional Django view',
+            'Gives you the most control over you application logic',
+            'Gives you the most control over you application logic',
+        ]
 
-    return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
-  def post(self, request):
-    """Create a hello message with our name"""
-    serializer = self.serializer_class(data=request.data)
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
 
-    if serializer.is_valid():
-      name = serializer.validated_data.get('name')
-      message = f'Hello {name}'
-      return Response({'message': message})
-    else:
-      return Response(
-        serializer.errors,
-        status=status.HTTP_400_BAD_REQUEST
-      )
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-  def put(self, request, pk=None):
-    """Handle updating an object"""
-    return Response({'method': 'PUT'})
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
 
-  def patch(self, request, pk=None):
-    """Parcial update of an object"""
-    return Response({'method': 'PATCH'})
+    def patch(self, request, pk=None):
+        """Parcial update of an object"""
+        return Response({'method': 'PATCH'})
 
-  def delete(self, request, pk=None):
-    """Delete an object"""
-    return Response({'method': 'DELETE'})
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
 
 
 class HelloViewSet(viewsets.ViewSet):
-  """Test API ViewSet"""
-  serializer_class = serializers.HelloSerializer
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
 
-  def list(self, request):
-    """Return a hello message"""
+    def list(self, request):
+        """Return a hello message"""
 
-    a_viewset = [
-      'Uses HTTP methods as function (get, post, patch, put, delete)',
-      'Is similiar to a traditional Django view',
-      'Gives you the most control over you application logic',
-      'Gives you the most control over you application logic',
-    ]
+        a_viewset = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similiar to a traditional Django view',
+            'Gives you the most control over you application logic',
+            'Gives you the most control over you application logic',
+        ]
 
-    return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
 
-  def create(self, request):
-    """Create a new hello message"""
+    def create(self, request):
+        """Create a new hello message"""
 
-    serializer = self.serializer_class(data=request.data)
+        serializer = self.serializer_class(data=request.data)
 
-    if serializer.is_valid():
-      name = serializer.validated_data.get('name')
-      message = f'Hello {name}'
-      return Response({'message': message})
-    else:
-      return Response(
-        serializer.errors,
-        status=status.HTTP_400_BAD_REQUEST
-      )
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-  """Handle creating and updating profiles"""
-  serializer_class = serializers.UserProfileSerializer
-  queryset = models.UserProfile.objects.all()
-  authentication_classes = (TokenAuthentication,)
-  permission_classes = (permissions.UpdateOwnProfile,)
-  filter_backends = (filters.SearchFilter,)
-  search_fields = ('name', 'email',)
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
